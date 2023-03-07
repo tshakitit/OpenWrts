757e9204cb9a4971eb88198ffce9941408882f96
tshakitit <104494470+tshakitit@users.noreply.github.com>
2023-03-07 18:42:37 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -17,5 +17,5 @@ git clone https://github.com/xiaorouji/openwrt-passwall.git package/PWpackages
 git clone -b luci https://github.com/xiaorouji/openwrt-passwall.git package/passwall
 
 # 替换默认主题
-rm -rf package/lean/luci-theme-argon 
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  package/lean/luci-theme-argon
+#rm -rf package/lean/luci-theme-argon 
+#git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  package/lean/luci-theme-argon
