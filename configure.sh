161155ad9052a876b8ca3083a07f1ce665fbee04
tshakitit <104494470+tshakitit@users.noreply.github.com>
2023-03-07 18:12:12 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -5,14 +5,16 @@
 sed -i 's/192.168.1.1/192.168.2.250/g' package/base-files/files/bin/config_generate 
 
 # Hello World
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 
 # luci-theme-infinityfreedom
 echo 'src-git infinityfreedom https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git' >>feeds.conf.default
 
 # passwall
-echo "src-git PWpackages https://github.com/xiaorouji/openwrt-passwall.git;packages" >> feeds.conf.default
-echo "src-git PWluci https://github.com/xiaorouji/openwrt-passwall2.git" >> feeds.conf.default
+#echo "src-git PWpackages https://github.com/xiaorouji/openwrt-passwall.git;packages" >> feeds.conf.default
+#echo "src-git PWluci https://github.com/xiaorouji/openwrt-passwall2.git" >> feeds.conf.default
+git clone https://github.com/xiaorouji/openwrt-passwall.git package/luci-app-passwall
+curl -o https://github.com/xiaorouji/openwrt-passwall/tree/luci/luci-app-passwall package/luci-app-passwall
 
 # 替换默认主题
 rm -rf package/lean/luci-theme-argon 
