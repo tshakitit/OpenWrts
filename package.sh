467d41011d3f9eacbf9bdc5ba50f2586dc8654b4
tshakitit <104494470+tshakitit@users.noreply.github.com>
2023-03-07 18:13:04 +0800

    Update package.sh

--- package.sh
+++ package.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-git clone  https://github.com/bigbugcc/OpenwrtApp package/otherapp/OpenwrtApp
-git clone  https://github.com/destan19/OpenAppFilter package/otherapp/OpenAppFilter
-git clone  https://github.com/zzsj0928/luci-app-pushbot package/otherapp/luci-app-pushbot
+#git clone  https://github.com/bigbugcc/OpenwrtApp package/otherapp/OpenwrtApp
+#git clone  https://github.com/destan19/OpenAppFilter package/otherapp/OpenAppFilter
+#git clone  https://github.com/zzsj0928/luci-app-pushbot package/otherapp/luci-app-pushbot
 # vssr
-git clone https://github.com/jerrykuku/lua-maxminddb.git package/otherapp/lua-maxminddb
-git clone https://github.com/jerrykuku/luci-app-vssr.git package/otherapp/luci-app-vssr
+#git clone https://github.com/jerrykuku/lua-maxminddb.git package/otherapp/lua-maxminddb
+#git clone https://github.com/jerrykuku/luci-app-vssr.git package/otherapp/luci-app-vssr
 
 # Theme
 # luci-theme-neobird
-git clone https://github.com/thinktip/luci-theme-neobird.git package/otherapp/luci-theme-neobird
+#git clone https://github.com/thinktip/luci-theme-neobird.git package/otherapp/luci-theme-neobird
 git clone https://github.com/kenzok8/luci-theme-ifit.git package/otherapp/luci-theme-ifit
 
 # Mentohust
-git clone https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk.git package/otherapp/mentohust
+#git clone https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk.git package/otherapp/mentohust
 
 # homeproxy
-git clone https://github.com/immortalwrt/homeproxy.git package/otherapp/luci-app-homeproxy
+#git clone https://github.com/immortalwrt/homeproxy.git package/otherapp/luci-app-homeproxy
