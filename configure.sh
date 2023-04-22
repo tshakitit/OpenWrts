6af97c013efa22ecb083096b21d9c612820c8722
tshakitit <104494470+tshakitit@users.noreply.github.com>
2023-04-22 17:26:41 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -8,13 +8,13 @@ sed -i 's/192.168.1.1/192.168.2.251/g' package/base-files/files/bin/config_gener
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 
 # luci-theme-infinityfreedom
-echo 'src-git infinityfreedom https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git' >>feeds.conf.default
+#echo 'src-git infinityfreedom https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git' >>feeds.conf.default
 
 # passwall
-echo "src-git PWpackages https://github.com/xiaorouji/openwrt-passwall.git;packages" >> feeds.conf.default
+#echo "src-git PWpackages https://github.com/xiaorouji/openwrt-passwall.git;packages" >> feeds.conf.default
 #echo "src-git PWluci https://github.com/xiaorouji/openwrt-passwall2.git" >> feeds.conf.default
-git clone https://github.com/xiaorouji/openwrt-passwall.git package/PWpackages
-git clone -b luci https://github.com/xiaorouji/openwrt-passwall.git package/passwall
+#git clone https://github.com/xiaorouji/openwrt-passwall.git package/PWpackages
+#git clone -b luci https://github.com/xiaorouji/openwrt-passwall.git package/passwall
 
 # 替换默认主题
 #sed -i "s/bootstrap/argon/ig" feeds/luci/collections/luci/Makefile
